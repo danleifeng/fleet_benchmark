0ebce47bbfaf0132ece4c7893c1f5c315b124873
danleifeng <danleifeng@163.com>
2020-01-12 12:42:04 +0000

    set cudnn

--- resnet/scripts/train_gpu.sh
+++ resnet/scripts/train_gpu.sh
@@ -2,10 +2,10 @@
 #sleep 10m
 set -x
 export FLAGS_sync_nccl_allreduce=1
-export FLAGS_cudnn_exhaustive_search=1
-export FLAGS_conv_workspace_size_limit=3000 #MB
+export FLAGS_cudnn_exhaustive_search=0
+#export FLAGS_conv_workspace_size_limit=3000 #MB
 export FLAGS_cudnn_batchnorm_spatial_persistent=1
-export FLAGS_fraction_of_gpu_memory_to_use=0.98
+#export FLAGS_fraction_of_gpu_memory_to_use=0.98
 
 #export GLOG_v=1
 export GLOG_logtostderr=1
