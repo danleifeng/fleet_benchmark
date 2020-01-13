f02814e523aaf6b5b1eeaa63dbdfd9820e22ec35
danleifeng <danleifeng@163.com>
2020-01-13 05:07:36 +0000

    rm export

--- resnet/scripts/train_gpu.sh
+++ resnet/scripts/train_gpu.sh
@@ -7,14 +7,14 @@ export FLAGS_cudnn_exhaustive_search=0
 export FLAGS_cudnn_batchnorm_spatial_persistent=1
 #export FLAGS_fraction_of_gpu_memory_to_use=0.98
 
-#export GLOG_v=1
+export GLOG_v=3
 export GLOG_logtostderr=1
 export FLAGS_eager_delete_tensor_gb=0
 export NCCL_DEBUG=INFO
 
-ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/lib/x86_64-linux-gnu/libcudnn.so
-export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64/:/usr/lib/x86_64-linux-gnu/:/usr/lib64/:/usr/local/lib/:$LD_LIBRARY_PATH
-export PADDLE_PSERVER_PORT=9184
+#ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/lib/x86_64-linux-gnu/libcudnn.so
+#export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64/:/usr/lib/x86_64-linux-gnu/:/usr/lib64/:/usr/local/lib/:$LD_LIBRARY_PATH
+#export PADDLE_PSERVER_PORT=9184
 
 MODEL=ResNet50 #VGG16
 MODEL_SAVE_PATH="output/"
